14c9c0180bc4341a8264fafa61cda9160450a399
chuckz <chuckz.espanola99@gmail.com>
2026-07-23 01:29:56 +0800

    feat: add initial Vaadin dashboard route

---
+++ src/main/java/com/transit/arctransit/ui/DashboardView.java
@@ -0,0 +1,14 @@
+package com.transit.arctransit.ui;
+
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("dashboard")
+public class DashboardView extends VerticalLayout {
+
+    public DashboardView() {
+        add(new H1("This is a test for arc transit system vaadin dashboard"));
+    }
+
+}
