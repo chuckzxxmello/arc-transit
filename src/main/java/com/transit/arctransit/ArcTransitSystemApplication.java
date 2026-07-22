a893a9d08beccb132e7b079c227e29a249d7f843
chuckz <chuckz.espanola99@gmail.com>
2026-07-22 23:18:26 +0800

    initialize Arc Transit System

---
+++ src/main/java/com/transit/arctransit/ArcTransitSystemApplication.java
@@ -0,0 +1,13 @@
+package com.transit.arctransit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcTransitSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArcTransitSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/transit/arctransit/ArcTransitSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transit.arctransit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcTransitSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
